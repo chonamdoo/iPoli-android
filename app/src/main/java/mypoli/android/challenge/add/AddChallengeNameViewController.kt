8ae9e162200ff795a7b30fb2cffacdfc82c22612
Polina Zhelyazkova <poly_vjk@abv.bg>
2018-03-08 16:54:39 +0200

    Create toolbar with no elevation

--- app/src/main/java/mypoli/android/challenge/add/AddChallengeNameViewController.kt
+++ app/src/main/java/mypoli/android/challenge/add/AddChallengeNameViewController.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.view_no_elevation_toolbar.view.*
 import mypoli.android.R
 import mypoli.android.challenge.add.AddChallengeNameViewState.StateType.LOADING
 import mypoli.android.common.AppState
@@ -11,6 +12,9 @@ import mypoli.android.common.BaseViewStateReducer
 import mypoli.android.common.mvi.ViewState
 import mypoli.android.common.redux.Action
 import mypoli.android.common.redux.android.ReduxViewController
+import mypoli.android.common.view.setToolbar
+import mypoli.android.common.view.showBackButton
+import mypoli.android.common.view.toolbarTitle
 
 /**
  * Created by Polina Zhelyazkova <polina@mypoli.fun>
@@ -59,7 +63,14 @@ class AddChallengeNameViewController(args: Bundle? = null) :
         savedViewState: Bundle?
     ): View {
         val view = inflater.inflate(R.layout.controller_add_challenge_name, container, false)
+        setToolbar(view.toolbar)
+        toolbarTitle = "New challenge"
         return view
+    }
+
+    override fun onAttach(view: View) {
+        super.onAttach(view)
+        showBackButton()
     }
 
     override fun render(state: AddChallengeNameViewState, view: View) {
