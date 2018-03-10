5aa77299b80dd10dc9084a0cf2d3e45102bc3c0d
Venelin Valkov <venelin@curiousily.com>
2018-03-10 12:09:10 +0200

    Show sample Challenge

--- app/src/main/java/mypoli/android/challenge/list/ChallengeListViewController.kt
+++ app/src/main/java/mypoli/android/challenge/list/ChallengeListViewController.kt
@@ -1,7 +1,9 @@
 package mypoli.android.challenge.list
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.support.annotation.ColorRes
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -9,16 +11,16 @@ import android.view.View
 import android.view.ViewGroup
 import com.bluelinelabs.conductor.RouterTransaction
 import com.bluelinelabs.conductor.changehandler.FadeChangeHandler
+import com.mikepenz.iconics.IconicsDrawable
+import com.mikepenz.iconics.typeface.IIcon
+import com.mikepenz.ionicons_typeface_library.Ionicons
 import kotlinx.android.synthetic.main.controller_challenge_list.view.*
-import kotlinx.android.synthetic.main.controller_repeating_quest_list.view.*
 import kotlinx.android.synthetic.main.item_challenge.view.*
 import mypoli.android.R
 import mypoli.android.challenge.add.AddChallengeViewController
 import mypoli.android.common.redux.android.ReduxViewController
+import mypoli.android.common.view.*
 import mypoli.android.common.view.recyclerview.SimpleViewHolder
-import mypoli.android.common.view.rootRouter
-import mypoli.android.common.view.stringRes
-import mypoli.android.common.view.toolbarTitle
 
 /**
  * Created by Venelin Valkov <venelin@mypoli.fun>
@@ -55,6 +57,8 @@ class ChallengeListViewController(args: Bundle? = null) :
         return view
     }
 
+    override fun onCreateLoadAction() = ChallengeListAction.Load
+
     override fun onAttach(view: View) {
         super.onAttach(view)
         toolbarTitle = stringRes(R.string.drawer_challenges)
@@ -63,7 +67,7 @@ class ChallengeListViewController(args: Bundle? = null) :
     override fun render(state: ChallengeListViewState, view: View) {
         when (state) {
             is ChallengeListViewState.Changed -> {
-                (view.repeatingQuestList.adapter as ChallengeAdapter).updateAll(
+                (view.challengeList.adapter as ChallengeAdapter).updateAll(
                     state.toViewModels(
                         view.context
                     )
@@ -72,7 +76,12 @@ class ChallengeListViewController(args: Bundle? = null) :
         }
     }
 
-    data class ChallengeViewModel(val id: String, val name: String)
+    data class ChallengeViewModel(
+        val id: String,
+        val name: String,
+        val icon: IIcon,
+        @ColorRes val color: Int
+    )
 
     inner class ChallengeAdapter(private var viewModels: List<ChallengeViewModel> = listOf()) :
         RecyclerView.Adapter<SimpleViewHolder>() {
@@ -91,6 +100,15 @@ class ChallengeListViewController(args: Bundle? = null) :
             val view = holder.itemView
 
             view.cName.text = vm.name
+
+            view.cIcon.backgroundTintList =
+                ColorStateList.valueOf(colorRes(vm.color))
+            view.cIcon.setImageDrawable(
+                IconicsDrawable(view.context)
+                    .icon(vm.icon)
+                    .colorRes(R.color.md_white)
+                    .sizeDp(24)
+            )
         }
 
         override fun getItemCount() = viewModels.size
@@ -105,7 +123,10 @@ class ChallengeListViewController(args: Bundle? = null) :
         return challenges.map {
             ChallengeViewModel(
                 id = it.id,
-                name = it.name
+                name = it.name,
+                color = AndroidColor.valueOf(it.color.name).color500,
+                icon = it.icon?.let { AndroidIcon.valueOf(it.name).icon }
+                    ?: Ionicons.Icon.ion_android_clipboard
             )
         }
     }
